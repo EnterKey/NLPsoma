420f4c2cde9e5cb1b2735ad8788d03ab7d754002
bubae <widianpear@gmail.com>
2014-07-29 01:32:47 -0700

    bbu mongodb handler

--- routes/index.js
+++ routes/index.js
@@ -9,35 +9,51 @@ exports.main = function(req, res){
 };
 
 exports.insert_user = function(req, res){
-  mongodb_handler.insert_user(req, res);
+  mongodb_handler.insert_user(req.body, function(err, data){
+
+  });
 };
 
 exports.insert_pageEntry = function(req, res){
-  mongodb_handler.insert_pageEntry(req, res);
+  mongodb_handler.insert_pageEntry(req.body, function(err, data){
+
+  });
 };
 
 exports.insert_pageDir = function(req, res){
-  mongodb_handler.insert_pageDir(req, res);
+  mongodb_handler.insert_pageDir(req.body, function(err, data){
+
+  });
 };
 
 exports.get_pageEntry_list = function(req, res){
-  mongodb_handler.get_pageEntry_list(req, res);
+  mongodb_handler.get_pageEntry_list(req.body, function(err, data){
+
+  });
 };
 
 exports.get_pageDir_list = function(req, res){
-  mongodb_handler.get_pageDir_list(req, res);
+  mongodb_handler.get_pageDir_list(req.body, function(err, data){
+
+  });
 };
 
 exports.get_pageAll_list = function(req, res){
-  mongodb_handler.get_pageAll_list(req, res);
+  mongodb_handler.get_pageAll_list(req.body, function(err, data){
+
+  });
 };
 
 exports.remove_pageEntry = function(req, res){
-  mongodb_handler.remove_pageEntry(req, res);
+  mongodb_handler.remove_pageEntry(req.body, function(err, data){
+
+  });
 };
 
 exports.remove_pageDir = function(req, res){
-  mongodb_handler.remove_pageDir(req, res);
+  mongodb_handler.remove_pageDir(req.body, function(err, data){
+
+  });
 };
 
 // exports.set_list = function(req, res){
