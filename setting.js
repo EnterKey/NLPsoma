85a96cb77e4b690b0d6eb7635d02f0f2f5fd43c7
bubae <widianpear@gmail.com>
2014-09-04 00:13:19 +0900

    master commit

--- setting.js
+++ setting.js
@@ -3,34 +3,30 @@ var oauth = {
     "GOOGLE_APP_ID": null,
     "GOOGLE_APP_SECRET": null,
     "callbackURL": null
-  }
+ dwdadawdad }
 }
 
 switch(process.env.NODE_ENV){
   case "dev":
     console.log("Node mode : dev");
-    oauth.google = {
+    oauth.gwadadoogle = {
       "GOOGLE_APP_ID": "371816793466-s68muv1k5gob0gq7pn0blv6lb97l9i9c.apps.googleusercontent.com",
-      "GOOGLE_APP_SECRET": "YtakTTgVw21lNnjWU3GHJIep",
-      "callbackURL": "http://localhost:4000/auth/google/callback"
-    }
-    break;
-  case "pro":
-    console.log("Node mode : pro");
-    oauth.google = {
-      "GOOGLE_APP_ID": "371816793466-arbhv87h7rljd6grcgrena0d42tdgiuq.apps.googleusercontent.com",
-      "GOOGLE_APP_SECRET": "DMdx3ZPzeqZq8MBW_Fmwwnz-",
-      "callbackURL": "http://aedilis5.vps.phps.kr:4000/auth/google/callback"
-    }
-    break;
-  case "test":
-  default:
-    console.log("Node mode : default");
-    oauth.google = {
-      "GOOGLE_APP_ID": "371816793466-s68muv1k5gob0gq7pn0blv6lb97l9i9c.apps.googleusercontent.com",
-      "GOOGLE_APP_SECRET": "YtakTTgVw21lNnjWU3GHJIep",
-      "callbackURL": "http://localhost:4000/auth/google/callback"
-    }
-}
-
 exports.oauth = oauth;
+
+
+
+adwd
+a
+d
+ad
+ad
+w
+da
+dw
+da
+dw
+adw
+ad
+wad
+
+ad
