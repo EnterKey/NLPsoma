655d3db20bffad74594ade949c33be81ad8b341b
earl31 <hanulhalim31@korea.ac.kr>
2014-11-16 21:21:46 +0900

    new bookmark

--- public/javascript/bookmark.js
+++ public/javascript/bookmark.js
@@ -2,7 +2,7 @@
  * Created by Earl-PC on 2014. 9. 24..
  */
 
-global_current_path = "/";
+var global_current_path="/";
 
 var bookmarkModel = {
 
@@ -37,15 +37,20 @@ var bookmarkModel = {
     },
 
     getContentData : function(pathdata){
-        var jquery_obj= $(this);
-        var name=jquery_obj.text().trim();
-        var path=jquery_obj.data('path');
-        if(path==null) path="/";
-        path=path+(name=="/"?"":name);
-        path=path&&path.trim()!="undefined"?path:"/"
-        if(path=="/"&& typeof(pathdata)=="string")
+        var jquery_obj,name,path;
+        if(typeof(pathdata)!="string") {
+            jquery_obj = $(pathdata).find('span');
+            name = jquery_obj.text().trim();
+            path = jquery_obj.data('path');
+            if (path == null) path = "/";
+            path = path + (name == "/" ? "" : name);
+            path = path && path.trim() != "undefined" ? path : "/"
+        }
+        else
             path=pathdata
+
         global_current_path=path;
+
         var params={
             path:path
         }
@@ -54,7 +59,7 @@ var bookmarkModel = {
             data:params,
             success : function(data) {
                 bookmarkController.contents_update(data);
-                bookmarkUIManager.addContentBookmarkUI();
+                bookmarkUIManager.addContentBookmarkUI(pathdata);
             }
         })
     },
@@ -72,11 +77,12 @@ var bookmarkModel = {
             data:params,
             success : function(data) {
                 bookmarkController.update();
+                bookmarkModel.getBookmarkData(global_current_path);
             }
         })
     },
 
-    addNewSubFolderData : function(jquery_obj,name){
+/*    addNewSubFolderData : function(jquery_obj,name){
         var a_obj=jquery_obj.find('a')
         var path=a_obj.data('path')+a_obj.text()
         path=path&&path.trim()!="undefined"?path:"/"
@@ -93,7 +99,7 @@ var bookmarkModel = {
                 bookmarkController.update();
             }
         })
-    },
+    },*/
 
     renameFolderData : function(){
         var params={
@@ -108,30 +114,28 @@ var bookmarkModel = {
         })
     },
 
-    renameFileData : function(){
+    renameFileData : function(_data){
+        console.log("clicked")
+        var contents_data={};
+        contents_data.pageInfo={};
+        var a_obj=$(this).parent().find('.link_btn');
+        contents_data.pageInfo.url=a_obj.attr('link')
+        a_obj=$(this).parent();
+        contents_data.pageInfo.path=a_obj.data('path')
+
+        bookmarkModel.getName(function(data){
+            contents_data.pageInfo.new_title = data;
+        })
+
         var params={
-            pageInfo:data.pageInfo
+            pageInfo:contents_data.pageInfo
         }
         $.ajax({
             url:"/ajax/rename_pageEntry",
             data:params,
             success : function() {
-                bookmarkController.update();
-            }
-        })
-    },
-
-    deleteFileData : function(){
-        var data = {}
-        data.pageInfo
-        var params={
-            pageInfo:data.pageInfo
-        }
-        $.ajax({
-            url:"/ajax/remove_pageEntry", //only dir
-            data:params,
-            success : function (){
-                bookmarkController.update();
+                bookmarkController.contents_update();
+                bookmarkModel.getContentData(global_current_path);
             }
         })
     },
@@ -147,6 +151,39 @@ var bookmarkModel = {
                 bookmarkController.update();
             }
         })
+    },
+
+    deleteFileData : function(_data){
+        var contents_data={};
+        contents_data.pageInfo={};
+        var a_obj=$(this).parent().find('.link_btn');
+        contents_data.pageInfo.url=a_obj.attr('link')
+        a_obj=$(this).parent();
+        contents_data.pageInfo.path=a_obj.data('path')
+
+        var params={
+            pageInfo:contents_data.pageInfo
+        }
+        $.ajax({
+            url:"/ajax/remove_pageEntry", //only dir
+            data:params,
+            success : function (){
+                bookmarkController.contents_update();
+                bookmarkModel.getContentData(global_current_path);
+            }
+        })
+    },
+
+    getName : function(callback){
+        var result = prompt("새 이름 입력","");
+
+        var alphaExp = /^[a-zA-Z0-9_-]+$/;
+        if(result.match(alphaExp)) {
+            callback(result);
+        }
+        else{
+            alert("File name contains bad character");
+        }
     }
 
 };
@@ -170,25 +207,37 @@ var bookmarkUIManager = {
     data : {},
     contents_data : {},
 
+    update : function(data){
+        this.data = data;
+    },
+
+    contents_update : function(data){
+        this.contents_data = data;
+    },
+
     init : function () {
         this.attachEvent();
     },
 
     addNavbarBookmarkUI : function(){
         $('.dir_only_elem').remove();
-        console.log(this.data);
         var result=this.data.data.pageDir;
         var innerhtml_str=""
-        innerhtml_str+="<p class='centered'>"+
-            "<a href=''>"+"<img src='/assets/img/ui-sam.jpg' class='img-circle' width='60'>"+"</a>"+"</p>"
-            +"<div class='row'>"+"<button type='button' class='btn btn-theme btn-newfolder'>"+"<i class='fa fa-check'>"+"</i>"+"새 폴더"+"</button>"+"<button type='button' class='btn btn-theme btn-delfolder'>"+"<i class='fa fa-check'>"+"</i>"+"지우기"+"</button>"+"</div>";
-        console.log(result);
+        innerhtml_str+="<button type='button' class='btn btn-theme folder_make_btn' style=''>"+
+            "새 폴더"+
+            "</button>";
+
         result.forEach(function(indata){
-            innerhtml_str+="<li class='mt'>"+
+            innerhtml_str+="<li class='mt droppable_binding'>"+
                 "<a data-path='&&dirpath&&'>"+
                 "<span>"+
                 "&&dirname&&"+
                 "</span>"+
+                "<div class='btn-group' style='float:right'>"+
+                "<button type='button' class='btn btn-theme dropdown-toggle dropdown_btn' data-toggle='dropdown'>"+
+                "관리"+ "<span class='caret'>"+"</span>"+
+                "</button>"+
+                "</div>"+
                 "</a>"+
                 "</li>";
             innerhtml_str=innerhtml_str.replace('&&dirname&&',indata.name).replace('&&dirpath&&',indata.path)
@@ -199,72 +248,109 @@ var bookmarkUIManager = {
         this.init();
     },
 
-    addContentBookmarkUI : function(){
+    addContentBookmarkUI : function(path){
+        console.log(this.contents_data)
         var resultEntry = this.contents_data.data.pageEntry;
 
         var innerhtml_str="";
 
         resultEntry.forEach(function(indata){
-            innerhtml_str+="<div class='list-group'>"+
-                "<a href='&&entryurl&&' class='list-group-item' data-path='&&entrypath&&'>"+
+            innerhtml_str+="<div class='list-group draggable_file'>"+
+                "<a class='list-group-item file_a_tag_title' data-path='&&entrypath&&'>"+
                 "<h3 class='list-group-item-heading'>"+
                 "&&title&&"+
+                "<button type='button' class='btn btn-theme delete_btn' style='float:right'>"+
+                "지우기"+
+                "</button>"+
+                "<button type='button' class='btn btn-theme rename_btn' style='float:right'>"+
+                "새 이름"+
+                "</button>"+
+                "<button type='button' class='btn btn-theme link_btn' style='float:right' link='&&entryurl&&'>"+
+                "링크"+
+                "</button>"+
                 "</h3>"+
                 "<p class='list-group-item-text'>"+
                 "&&content&&"+
                 "</p>"+
                 "</a>"+
-                "<div class='row'>"+
-                "<button type='button' class='btn btn-theme delete_btn'>"+
-                "지우기"+
-                "</button>"+
-                "<button type='button' class='btn btn-theme rename_btn'>"+
-                "새 이름"+
-                "</button>"+
-                "</div>"+
                 "</div>";
             innerhtml_str=innerhtml_str.replace(/&&entryurl&&/g,indata.url).replace(/&&entrypath&&/g,indata.path).replace(/&&title&&/g,indata.title).replace(/&&content&&/g,indata.content.substring(0,200));
         })
 
         document.getElementById("bookmark_menu").innerHTML=innerhtml_str;
+
         this.init();
+
+       // this.addSubFolderViewerUI(path);
+
     },
 
-    addSubFolderViewerUI : function(){
+/*    addSubFolderViewerUI : function(path){
+        var resultDir = bookmarkUIManager.contents_data.data.pageDir;
+        var innerhtml_str="";
 
-    },
+        resultDir.forEach(function(indata){
+            innerhtml_str+="<li class='mt'>"+
+                "<a data-path='&&dirpath&&'>"+
+                "<span>"+
+                "&&dirname&&"+
+                "</span>"+
+                "</a>"+
+                "</li>";
+            innerhtml_str=innerhtml_str.replace(/&&dirname&&/g,indata.name).replace(/&&dirpath&&/g,indata.path)
+        })
 
-    update : function(data){
-        this.data = data;
-    },
-
-    contents_update : function(data){
-        this.contents_data = data;
-    },
+        $(path).append(innerhtml_str);
+    },*/
 
     attachEvent : function () {
         this._NavbarOffClickEvent();
         this._NavbarClickEvent();
+        this._AddNewFolderOffClickEvent();
+        this._AddNewFolderClickEvent();
         this._RenameBtnOffClickEvent();
         this._RenameBtnClickEvent();
         this._DeleteBtnOffClickEvent();
         this._DeleteBtnClickEvent();
+        this._LinkBtnOffClickEvent();
+        this._LinkBtnClickEvent();
+        this._DropdownBindingOffEvent();
+        this._DropdownBindingEvent();
+        this._DraggableBindingEvent();
+        this._DroppableBindingEvent();
     },
 
     _NavbarClickEvent : function(){
-        $('li.mt').on('click', bookmarkModel.getContentData);
+        $('li.mt').on('click', function(){
+            bookmarkModel.getContentData(this);
+        });
+
     },
 
     _NavbarOffClickEvent : function(){
-        $('li,mt').off('click');
+        $('li.mt').off('click');
+    },
+
+    _AddNewFolderOffClickEvent : function(){
+        $('.folder_make_btn').off('click');
+    },
+
+    _AddNewFolderClickEvent : function(){
+        $('.folder_make_btn').on('click',function(){
+            var name;
+            bookmarkModel.getName(function(data){
+                name=data;
+            })
+            bookmarkModel.addNewFolderData(name,global_current_path);
+        });
     },
 
     _RenameBtnClickEvent : function(){
-        //$('rename_btn').on('click',)
+        $('.rename_btn').on('click',bookmarkModel.renameFileData);
     },
 
     _RenameBtnOffClickEvent : function(){
-        $('rename_btn').off('click');
+        $('.rename_btn').off('click');
     },
 
     _DeleteBtnClickEvent : function(){
@@ -273,28 +359,74 @@ var bookmarkUIManager = {
 
     _DeleteBtnOffClickEvent : function(){
         $('.delete_btn').off('click');
+    },
+
+    _LinkBtnOffClickEvent : function(){
+        $('.link_btn').off('click');
+    },
+
+    _LinkBtnClickEvent : function(){
+        $('.link_btn').on('click',function(){
+            console.log($(this).attr('link'))
+            window.open($(this).attr('link'),"새 창");
+        });
+    },
+
+    _DropdownBindingOffEvent : function(){
+    },
+
+    _DropdownBindingEvent : function(){
+         var oDropdown = $("#fragment.dropdown-menu");
+         $('.dropdown_btn').on('click', function(){
+            $(this).parent().append(oDropdown);
+            oDropdown.show();
+         })
+    },
+
+    _DraggableBindingEvent : function(){
+        $('.draggable_file').draggable({
+            start:function(e,u){
+                //save dragged item
+            },
+            drag:function(e,u){
+            },
+            stop:function(e,u){
+                //check on the folder or not
+
+                //if on the folder
+                    //remove dragged item
+                    //change path of item
+            },
+            revert: true,
+            opacity:"0.3"
+        });
+    },
+
+    _DroppableBindingEvent : function() {
+        $('.droppable_binding').droppable({
+            drop : function(event, ui) {
+                var jquery_obj = $(this).find('a');
+                var dragobj = $(ui.draggable);
+                var params = {
+                    pageInfo: {
+                        oldPath: dragobj.find('.file_a_tag_title').data('path'),
+                        newPath: jquery_obj.data('path') + jquery_obj.find('span').text().trim(),
+                        url: dragobj.find('.link_btn').attr('link')
+                    }
+                };
+
+                $.ajax({
+                    url: "/ajax/move_entryPath",
+                    data: params,
+                    success: function (data) {
+                        dragobj.remove()
+                        bookmarkModel.getContentData(global_current_path);
+                    }
+                })
+            }
+        })
     }
 
-    /*_DropdownEvent : function(){
-     var oDropdown = $("#fragment.item-dropdown");
-     $('.dropdown_button').on('click', function(){
-     $(this).parent().append(oDropdown);
-     oDropdown.show();
-     })
-     },
-
-     _LongNameHoverAnimationEvent : function(){
-     var long_name_animateright=function(){
-     $(this).animate({scrollLeft:this.scrollWidth-$(this).width()}, (this.scrollWidth-$(this).width())*15);
-     $(this).one('mouseleave',long_name_animateleft)
-     }
-     var long_name_animateleft=function(){
-     $(this).animate({scrollLeft:0}, (this.scrollWidth-$(this).width())*15);
-     $(this).one('mouseover',long_name_animateright)
-     }
-     $('.long_name_overflow').one('mouseover',long_name_animateright)
-     $('.long_name_overflow').one('mouseleave',long_name_animateleft)
-     }*/
 };
 
 
